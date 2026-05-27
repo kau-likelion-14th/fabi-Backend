f5b032bc21d5aac551bbdbed33e0e3873a490508
e2guana <leedotol@naver.com>
2026-05-27 14:56:07 +0900

    docs/springcode

--- src/main/java/likelion14th/lte/user/controller/UserProfileController.java
+++ src/main/java/likelion14th/lte/user/controller/UserProfileController.java
@@ -14,28 +14,36 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@Slf4j
 @RequestMapping("/api/profile")
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserProfileController {
+public class UserProfileController{
+
     public final UserProfileService userProfileService;
 
-    @GetMapping
-    @Operation(summary = "유저 프로필 조회", description = "유저아이디를 받아 유저 프로필을 반환하는 api")
-    public ApiResponse<UserProfileResponse> getUserProfile(
-            @RequestParam Long userId){
-            UserProfileResponse userProfileResponse = userProfileService.getUserProfile(userId);
+    // [Q9. Controller 내부에서 userRepository.findById()를 직접 호출해서 유저를 찾지 않고,
+    // 반드시 userProfileService를 호출하여 작업을 위임해야 하는 이유는 무엇인가요? (단일 책임 원칙 관점)]
+    /** 답변:
+     * Service에 작업을 위임하면 Controller는 API I/O만 담당하고 실제 로직은 Service에서
+     * 처리가 이루어지기 때문에 계층별 역할이 명확해져 유지보수가 용이하다.
+     */
 
-            return ApiResponse.onSuccess(SuccessCode.OK, userProfileResponse);
+    @GetMapping
+    public ApiResponse<UserProfileResponse> getUserProfile(@RequestParam Long userId){
+        UserProfileResponse response = userProfileService.getUserProfile(userId);
+        return ApiResponse.onSuccess(SuccessCode.USER_INFO_GET_SUCCESS, response);
     }
 
     @PostMapping
-    @Operation(summary = "테스트 유저 생성", description = "이름, 한줄소개, 유저 태그")
-    public ApiResponse<UserProfileResponse> createTestUserProfile(
-            @RequestBody CreateTestUserRequest createTestUserRequest
-            ){
-        UserProfileResponse response = userProfileService.createTestUser(createTestUserRequest);
-
+    public ApiResponse<UserProfileResponse> createTestUser(
+            // [Q10. 클라이언트가 보낸 JSON 텍스트 데이터가 어떻게 자바 객체인 CreateTestUserRequest로
+            // 변환 되는지앞의 어노테이션과 연관 지어 설명해 보세요.]
+            /** 답변:
+             * @RequestBody가 HTTP request body에 담긴 JSON을 자바 객체로 변환해준다
+             * Spring이 내부적으로 JSON의 key랑 자바의 필ㄷ를 매핑한다.
+             */
+            @RequestBody CreateTestUserRequest request
+    ){
+        UserProfileResponse response = userProfileService.createTestUser(request);
         return ApiResponse.onSuccess(SuccessCode.CREATED, response);
     }
 }
--- src/main/java/likelion14th/lte/user/service/UserProfileService.java
+++ src/main/java/likelion14th/lte/user/service/UserProfileService.java
@@ -15,30 +15,58 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserProfileService {
+public class UserProfileService{
+
+    // [Q5. Service 안에서 new UserRepository() 로 객체를 직접 생성하지 않고,
+    // 외부에서 의존성 주입(DI)을 받는 이유는 무엇인가요? (결합도와 단위 테스트 관점)]
+    /** 답변:
+     * 코드의 결합도가 높아지는 것을 방지하기 위함
+     * 단위 테스트시 임의로 객체를 넣기 어려움
+     */
     private final UserRepository userRepository;
 
-    @Transactional
-    public UserProfileResponse createTestUser(CreateTestUserRequest request){
-        User newUser = User.builder()
-                .username(request.getUsername())
-                .userTag(request.getUserTag())
-                .introduction(request.getIntroduction())
-                .build();
-        User savedUser;
-        try{
-            savedUser = userRepository.save(newUser);
-        }
-        catch(Exception e){
-            throw new GeneralException(ErrorCode.BAD_REQUEST);
-        }
-        return UserProfileResponse.from(savedUser);
-    }
+    // [Q6. (코딩 문제) 만약 클래스 위의 @RequiredArgsConstructor를 지운다면,
+    // 우리가 직접 작성해야 할 의존성 주입용 자바 '생성자' 코드는 어떤 모습일까요? 아래에 직접 코딩해 보세요.]
+    /**
+       여기에 생성자 코드 작성:
+        public UserProfileService(UserRepository userRepository){
+            this.userRepository = userRepostory;
+     }
+
+    */
 
     @Transactional(readOnly = true)
     public UserProfileResponse getUserProfile(Long userId){
         User user = userRepository.findById(userId)
-                .orElseThrow(()-> new GeneralException(ErrorCode.USER_NOT_FOUND));
+                .orElseThrow(() -> new GeneralException(ErrorCode.USER_NOT_FOUND));
         return UserProfileResponse.from(user);
+    }
+
+    @Transactional
+    public UserProfileResponse createTestUser(CreateTestUserRequest request){
+
+        // [Q7. 일반적인 생성자 new User(name, intro, tag) 방식을 쓰지 않고,
+        // User.builder()...build() 라는 '빌더 패턴'을 사용하여 객체를 조립했을 때 얻는 장점은 무엇인가요?]
+        /** 답변:
+         * 값이 어디에 들어가는지 명확하니까 가독성과 유지보수에 용이함
+         */
+        User newUser = User.builder()
+                .username(request.getUsername())
+                .introduction(request.getIntroduction())
+                .userTag(request.getUserTag())
+                .build();
+
+        User savedUser;
+        try{
+            // [Q8. 데이터를 저장하는 이 메서드 위에 @Transactional이 반드시 붙어야 하는 이유는 무엇인가요?
+            // (저장 도중 DB 서버가 끊겼을 때의 상황을 가정해서 설명하세요)]
+            /** 답변:
+             * 중간에 서버가 끊겼을 시 모든 작업을 되돌려 데이터 원자성 보장
+             */
+            savedUser = userRepository.save(newUser);
+        } catch (Exception e){
+            throw new GeneralException(ErrorCode.BAD_REQUEST);
+        }
+        return UserProfileResponse.from(savedUser);
     }
 }

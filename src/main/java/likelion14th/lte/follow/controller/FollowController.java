64dc4745bcf423639d7e07b8fbd5687c8223b754
e2guana <leedotol@naver.com>
2026-07-08 18:26:25 +0900

    과제 api 추가

--- src/main/java/likelion14th/lte/follow/controller/FollowController.java
+++ src/main/java/likelion14th/lte/follow/controller/FollowController.java
@@ -17,6 +17,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @Slf4j
 @RequestMapping("api/follow")
@@ -36,6 +38,48 @@ public class FollowController {
         FollowUserResponse response = followService.followUser(userId, followUserRequest.getToUserId());
 
         return ApiResponse.onSuccess(SuccessCode.FOLLOW_ADD_SUCCESS, response);
+    }
+
+    @DeleteMapping
+    @Operation(summary = "언팔로우", description = "팔로우를 취소합니다.")
+    public ApiResponse<Void> deleteFollow(
+            @RequestParam Long userId,
+            @RequestBody FollowUserRequest followUserRequest
+    ){
+        followService.unfollowUser(userId, followUserRequest.getToUserId());
+
+        return ApiResponse.onSuccess(SuccessCode.FOLLOW_DELETE_SUCCESS, null);
+    }
+
+    @GetMapping("/followers")
+    @Operation(summary = "팔로워 목록 조회", description = "나를 팔로우하는 유저 목록을 조회합니다.")
+    public ApiResponse<List<FollowUserResponse>> getFollowers(
+            @RequestParam Long userId
+    ){
+        List<FollowUserResponse> responses = followService.getFollowers(userId);
+
+        return ApiResponse.onSuccess(SuccessCode.FOLLOW_LIST_GET_SUCCESS, responses);
+    }
+
+    @GetMapping("/followings")
+    @Operation(summary = "팔로잉 목록 조회", description = "내가 팔로우하는 유저 목록을 조회합니다.")
+    public ApiResponse<List<FollowUserResponse>> getFollowings(
+            @RequestParam Long userId
+    ){
+        List<FollowUserResponse> responses = followService.getFollowings(userId);
+
+        return ApiResponse.onSuccess(SuccessCode.FOLLOW_LIST_GET_SUCCESS, responses);
+    }
+
+    @GetMapping
+    @Operation(summary = "팔로우 가능 유저 목록 조회", description = "팔로우 가능한 유저 목록을 페이징하여 조회합니다.")
+    public ApiResponse<Page<FollowUserResponse>> getCanFollowUsers(
+            @RequestParam Long userId,
+            @ParameterObject @PageableDefault(size = 10, page = 0) Pageable pageable
+    ){
+        Page<FollowUserResponse> responses = followService.getCanFollowUsers(userId, pageable);
+
+        return ApiResponse.onSuccess(SuccessCode.FOLLOW_SEARCH_SUCCESS, responses);
     }
 
     @GetMapping("/search")

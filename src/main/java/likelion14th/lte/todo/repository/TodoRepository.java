e1d6a1e7e024906681d1b44069a36e268bf02eb8
e2guana <leedotol@naver.com>
2026-09-15 16:51:56 +0900

    oauth

--- src/main/java/likelion14th/lte/todo/repository/TodoRepository.java
+++ src/main/java/likelion14th/lte/todo/repository/TodoRepository.java
@@ -1,11 +1,13 @@
 package likelion14th.lte.todo.repository;
 
 import likelion14th.lte.todo.entity.Todo;
+import likelion14th.lte.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    List<Todo> findAllByUser(User user);
 
     //스캐줄러에서 루틴인 애들을 전부 싸그리 긁어다가 투두데이트 애들 쭉생성해줄려고
     List<Todo> findAllByRoutineEnabledTrue();
